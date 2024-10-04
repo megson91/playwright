1da3f3f870a93812b6ea5716d71758de5c4fce05
megson91 <m.piasecka7@gmail.com>
2024-10-04 13:53:57 +0200

    updates

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Pulpit tests', () => {
-    test.only('test', async ({ page }) => {
+    test('quick payment with correct data', async ({ page }) => {
         await page.goto('https://demo-bank.vercel.app/');
         await page.getByTestId('login-input').fill('Tester66');
         await page.getByTestId('password-input').fill('qwerty1234');
@@ -10,9 +10,29 @@ test.describe('Pulpit tests', () => {
 
         await page.locator('#widget_1_transfer_receiver').selectOption('2');
         await page.locator('#widget_1_transfer_amount').fill('150');
-        await page.locator('#widget_1_transfer_title').fill('Zwrot środków');
-        await page.getByRole('button', { name: 'wykonaj' }).click();
-        await page.getByTestId('close-button').click();
-    });
+        await page.locator('#widget_1_transfer_title').fill('pizza');
 
+        // await page.getByRole('button', { name: 'wykonaj' }).click();
+        await page.locator('#execute_btn').click();
+        await page.getByTestId('close-button').click();
+        
+        await expect(page.locator('#show_messages')).toHaveText('Przelew wykonany! Chuck Demobankowy - 150,00PLN - pizza');
+
+    });
+    test('top up phone with correct data', async ({ page }) => {
+        await page.goto('https://demo-bank.vercel.app/');
+        await page.getByTestId('login-input').fill('Tester66');
+        await page.getByTestId('password-input').fill('qwerty1234');
+        await page.getByTestId('login-button').click();
+        await page.getByTestId('user-name').click();
+
+        await page.locator('#widget_1_topup_receiver').selectOption('500 xxx xxx');
+        await page.locator('#widget_1_topup_amount').fill('66');
+        await page.locator('#uniform-widget_1_topup_agreement span').click();
+        await page.locator('#execute_phone_btn').click();
+        await page.getByTestId('close-button').click();
+
+        await expect(page.locator('#show_messages')).toHaveText('Doładowanie wykonane! 66,00PLN na numer 500 xxx xxx'); 
+    });
+    
 });
